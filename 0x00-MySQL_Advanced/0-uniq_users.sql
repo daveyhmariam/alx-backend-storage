da058c27df3058815344d6cd0bdff814e64de73b
daveyhmariam <daveyhmariam@gmail.com>
2024-09-09 22:05:56 +0300

    unique User

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,7 @@
 --- create table
 
-CREATE TABLE IF NOT EXISTS users(id INT UNIQUE NOT NULL AUTO_INCREMENT, email NOT NULL UNIQUE, name VARCHAR(225), PRIMARY_KEY(id));
+CREATE TABLE IF NOT EXISTS users(
+                                id INT PRIMARY_KEY NOT NULL AUTO_INCREMENT,
+                                email NOT NULL UNIQUE VARCHAR(255),
+                                name VARCHAR(225)
+                                );
